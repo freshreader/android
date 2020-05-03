b1f5c9890b51497d1c796af5a5fb82425e12ff6f
Maxime Vaillancourt <mv@maximevaillancourt.com>
2020-05-03 18:43:34 -0400

    v1.0.0

--- app/src/main/java/app/freshreader/android/MainActivity.kt
+++ app/src/main/java/app/freshreader/android/MainActivity.kt
@@ -1,67 +1,118 @@
 package app.freshreader.android
-
+import android.content.Context
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.github.kittinunf.fuel.httpGet
-import com.google.android.material.snackbar.Snackbar
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Headers
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result;
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        FuelManager.instance.basePath = "https://freshreader.app"
+        saveUriFromIntentIfAny()
+    }
+
+    private fun saveUriFromIntentIfAny() {
         val uriFromIntentData: String? = intent?.data?.toString()
         val uriFromClipData: String? = intent?.clipData?.getItemAt(0)?.text?.toString()
-        val textViewFirst: TextView = findViewById<TextView>(R.id.textview_activity)
+
+        val accountNumber = getSharedPrefs().getString(getString(R.string.shared_prefs_account_number_key), "")
+        val apiAuthToken = getSharedPrefs().getString(getString(R.string.shared_prefs_api_auth_token_key), "")
 
         val uriToSave = when {
-            !uriFromIntentData.isNullOrBlank() -> {
-                uriFromIntentData
-            }
-            !uriFromClipData.isNullOrBlank() -> {
-                uriFromClipData
-            }
+            !uriFromIntentData.isNullOrBlank() -> { uriFromIntentData }
+            !uriFromClipData.isNullOrBlank() -> { uriFromClipData }
             else -> null
         }
 
         if (uriToSave != null) {
-            textViewFirst.text = uriToSave.toString()
-
-            val accountNumber: String = "0000000000000000" // TODO: persist to (and fetch from) shared prefs
             val encodedUri: String = Uri.encode(uriToSave.toString())
-            val finalUri: String = "https://freshreader.app/save-mobile?account_number=$accountNumber&url=$encodedUri"
+            val uri = "/api/v1/articles?url=$encodedUri"
+            saveUri(uri, apiAuthToken, accountNumber)
+        }
+    }
 
-            val httpAsync = finalUri
-                .httpGet()
-                .responseString { _request, response, result ->
-                    if (result is com.github.kittinunf.result.Result.Success && response.statusCode == 200) {
-                        Toast.makeText(applicationContext,"Saved!", Toast.LENGTH_SHORT).show()
+    private fun saveUri(uri: String, apiAuthToken: String?, accountNumber: String?) {
+        Fuel
+            .post(uri)
+            .header(
+                Headers.AUTHORIZATION,
+                "Token token=\"${apiAuthToken}\", account_number=\"${accountNumber}\""
+            )
+            .responseJson { request, response, result ->
+                result.fold(success = { json ->
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.saved_toast_msg),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }, failure = { _error ->
+                    if (_error.response.statusCode == 401) {
+                        getNewApiTokenAndRetry(uri, accountNumber)
                     }
-                    else {
-                        Toast.makeText(applicationContext,"Failed to save.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.failed_to_save_toast_msg),
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                })
+                runOnUiThread(Runnable {
+                    run {
+                        finishAffinity()
                     }
-                    runOnUiThread(Runnable {
-                        run {
-                            finishAffinity()
+                })
+            }
+    }
+
+    private fun getNewApiTokenAndRetry(uriToSave: String, accountNumber: String?) {
+        Fuel.get("/api/v1/users/${accountNumber}").appendHeader(Headers.ACCEPT to "application/json")
+            .responseJson { request, response, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        Toast.makeText(
+                            applicationContext,
+                            getString(R.string.failed_to_save_toast_msg),
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                    is Result.Success -> {
+                        val newApiAuthToken = result.get().obj().get("api_auth_token").toString()
+
+                        with(getSharedPrefs().edit()) {
+                            putString(
+                                getString(R.string.shared_prefs_api_auth_token_key),
+                                newApiAuthToken
+                            )
+                            apply()
+                            Toast.makeText(
+                                applicationContext,
+                                "Account number saved. API token: $newApiAuthToken",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            saveUri(uriToSave, newApiAuthToken, accountNumber)
                         }
-                    })
+                    }
                 }
+            }
+    }
 
-            httpAsync.join()
-        }
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+    private fun getSharedPrefs(): SharedPreferences {
+        return getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -75,7 +126,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                Toast.makeText(applicationContext, getString(R.string.easter_egg), Toast.LENGTH_SHORT).show()
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
